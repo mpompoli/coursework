4ebb8c665b15da49fd6b8144a60479900738526b
Matteo Pompoli <mpompoli@gmail.com>
2011-05-04 22:22:09 +0200

    fix to first exercises

--- ltp/ch01/numbers02.rb
+++ ltp/ch01/numbers02.rb
@@ -1,5 +1,7 @@
 seconds_old = 1031 * 1000000
 
 # Replace "nil" with math to determine the number of years in seconds_old
-years_old = 41
+years_old = seconds_old / 60 / 60 / 24 / 365
 puts "Given I am #{seconds_old} seconds old, I am #{years_old} years old"
+
+
--- ltp/ch04/favorite_number.rb
+++ ltp/ch04/favorite_number.rb
@@ -2,6 +2,6 @@
 # add one to the number, then suggest the result as a bigger and better
 # favorite number. (Do be tactful about it, though.)
 puts 'What\'s your favorite number?'
-number = gets.chomp
+number = gets
 favorite = number.to_i + 1
 puts 'my favorite number' + favorite + 'is bigger' 
